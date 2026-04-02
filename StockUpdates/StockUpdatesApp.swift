1d114a274841e997a2d8478324d65391cfea959e
Darshan Mothreja <darshanmothreja@Darshans-MacBook-M4-Pro.local>
2026-04-02 12:27:45 +0530

    Created project with StockUpdates Name It also includes by default UI and Unit test cases files.

---
+++ StockUpdates/StockUpdatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockUpdatesApp.swift
+//  StockUpdates
+//
+//  Created by Darshan Mothreja on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct StockUpdatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
